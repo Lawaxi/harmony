7a245e4f08589b0c5647e31f7003e55bd4aa5a2c
Lawaxi <2330234142@qq.com>
2022-02-13 16:04:26 +0800

    2

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
 
 # Harmony
 
+[![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](https://github.com/imorland/level-ranks/blob/master/LICENSE) [![Latest Stable Version](https://img.shields.io/packagist/v/lawaxi/level-ranks.svg)](https://github.com/lawaxi/level-ranks)
+
 将未登录时的论坛伪装成博客 (用于中国大陆个人备案)
+
+### 安装 🎈
+
+使用 composer 安装:
+
+```sh
+composer require lawaxi/harmony:"*"
+```
+
+### 链接 🔗
+
+- [Github](https://github.com/lawaxi/harmony)
+- [Packagist](https://packagist.org/packages/lawaxi/harmony)
--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -5,12 +5,14 @@ import WelcomeHero from 'flarum/forum/components/WelcomeHero';
 import HeaderSecondary from 'flarum/forum/components/HeaderSecondary';
 import IndexPage from 'flarum/forum/components/IndexPage';
 import Button from 'flarum/common/components/Button';
+import Tag from 'flarum/tags/models/Tag';
+import Model from 'flarum/common/Model';
 
 app.initializers.add('lawaxi-harmony', (app) => {
 
   //文章列表操作
-  override(DiscussionListItem.prototype, 'view', function(view) {
-    if(!app.session.user) {
+  override(DiscussionListItem.prototype, 'view', function (view) {
+    if (!app.session.user) {
       if ((this.attrs.discussion.user().username() !== (app.forum.attribute("lawaxi-harmony.allown") || "delay"))) {
         return null;
       } else if (!(this.attrs.discussion.title().indexOf("习") === -1
@@ -23,12 +25,13 @@ app.initializers.add('lawaxi-harmony', (app) => {
         && this.attrs.discussion.title().indexOf("论坛") === -1
         && this.attrs.discussion.title().indexOf("社区") === -1)) {
         return null;
-      }
+      } else if(this.attrs.discussion.replyCount()>0)
+      {return null;}
     }
     return view();
   });
 
-  override(DiscussionListItem.prototype, 'infoItems', function(infoItems) {
+  override(DiscussionListItem.prototype, 'infoItems', function (infoItems) {
     let a = infoItems();
     if (!app.session.user) {
       a.remove("terminalPost");
@@ -36,16 +39,13 @@ app.initializers.add('lawaxi-harmony', (app) => {
     return a;
   });
 
-  override(DiscussionListItem.prototype, 'replyCountItem', function(replyCountItem) {
-    if (!app.session.user) {
-      return "";
-    }
-    return replyCountItem();
+  override(DiscussionListItem.prototype, 'replyCountItem', function (replyCountItem) {
+    if (!app.session.user) {return null;}
   });
 
   //Hero标题按钮
-  override(WelcomeHero.prototype, 'view', function(view) {
-    if(view() !== null && !app.session.user){
+  override(WelcomeHero.prototype, 'view', function (view) {
+    if (view() && !app.session.user) {
       const slideUp = () => {
         this.$().slideUp(this.hide.bind(this));
       };
@@ -72,25 +72,35 @@ app.initializers.add('lawaxi-harmony', (app) => {
   });
 
   //注册按钮(仍然可以通过登录按钮注册)
-  override(HeaderSecondary.prototype, 'items', function(view) {
-      let a = view();
-      if (!app.session.user) {
-        a.remove("signUp");
-      }
-      return a;
+  override(HeaderSecondary.prototype, 'items', function (view) {
+    let a = view();
+    if (!app.session.user) {
+      a.remove("signUp");
+    }
+    return a;
   });
 
   //发文章按钮
-  override(IndexPage.prototype, 'sidebarItems', function(sidebarItems) {
+  if(app.initializers.has('flarum-tags') && app.initializers.has('fof/best-answer')) {
+    Tag.prototype.isQnA = Model.attribute('isQnA');
+  }
+
+  override(IndexPage.prototype, 'sidebarItems', function (sidebarItems) {
     let a = sidebarItems();
+    if(app.initializers.has('flarum-tags') && app.initializers.has('fof/best-answer')){
+      if(this.currentTag()?.isQnA?.()){
+        return a;}
+    }
+
     if (!app.session.user) {
       a.remove("newDiscussion");
     }
     return a;
   });
 
+
   //选择器按钮
-  override(IndexPage.prototype, 'viewItems', function(viewItems) {
+  override(IndexPage.prototype, 'viewItems', function (viewItems) {
     let a = viewItems();
     if (!app.session.user) {
       a.remove("sort");
@@ -98,4 +108,16 @@ app.initializers.add('lawaxi-harmony', (app) => {
     return a;
   });
 
+  //去除问答用标签
+  if (app.initializers.has('flarum-tags') && app.initializers.has('fof/best-answer')) {
+    override(IndexPage.prototype, 'navItems', function (navItems) {
+      let a = navItems();
+      app.store.all('tags').forEach((value, index, array) => {
+        if(value?.isQnA?.()){
+          a.remove("tag"+value.id());
+        }
+      })
+      return a;
+    });
+  }
 });
