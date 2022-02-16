6e493167fc4ba5adba592262df063fd87ca62547
Lawaxi <2330234142@qq.com>
2022-02-16 20:59:31 +0800

    3.1.5

--- migrations/2021_01_17_000000_set_default_allown.php
+++ migrations/2021_01_17_000000_set_default_allown.php
@@ -9,7 +9,7 @@ return [
 
         $settings->set('lawaxi-harmony.allown', "delay");
         $settings->set('lawaxi-harmony.blogtitle', "六边形博客");
-        $settings->set('lawaxi-harmony.blogsubtitle', "“若夫学者，则无所不思，无所不言，以其无责，可以行其志也；若云思不出其位，是自弃于浅陋之学也。” 本站为 delay 个人学习生活兴趣分享博客。");
+        $settings->set('lawaxi-harmony.blogsubtitle', "“月亮在无人看时不存在。” 本站为 delay 个人学习生活兴趣分享博客。");
         $settings->set('lawaxi-harmony.ban', "习,江,彭,台,北京,扬州,续,论坛,社区");
         $settings->set('lawaxi-harmony.bantags', "teahouse");
     },
