b1c3efee4636a3f8bfb9852833c6dfa400ced7ca
Lawaxi <2330234142@qq.com>
2022-02-16 21:17:46 +0800

    3.1.9

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -35,9 +35,8 @@ app.initializers.add('lawaxi-harmony', (app) => {
 
         //敏感分类内的文章
         if(app.initializers.has('flarum-tags')){
-          let bantags = app.forum.attribute("lawaxi-harmony.bantags");
           this.attrs.discussion.tags().forEach((value) =>{
-            if(bantags.indexOf(value) !== -1){
+            if(app.forum.attribute("lawaxi-harmony.bantags").indexOf(value) !== -1){
               throw new Error('a');
             }
           })
@@ -148,7 +147,6 @@ app.initializers.add('lawaxi-harmony', (app) => {
   if(app.initializers.has('flarum-tags')) {
     override(IndexPage.prototype, 'navItems', function (navItems) {
       let a = navItems();
-      let bantags = app.forum.attribute("lawaxi-harmony.bantags");
       if (!app.session.user) {
         app.store.all('tags').forEach((value, index, array) => {
           if (app.initializers.has('fof/best-answer')) {
@@ -157,7 +155,7 @@ app.initializers.add('lawaxi-harmony', (app) => {
             }
           }
 
-          if (bantags.indexOf(value.slug()) !== -1) {
+          if (app.forum.attribute("lawaxi-harmony.bantags").indexOf(value.slug()) !== -1) {
             a.remove("tag" + value.id());
           }
 
